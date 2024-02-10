f95e14288cdb634f4e7b88824df71f223640db23
Ivan Sauchuk <67599547+siv11121993@users.noreply.github.com>
2024-02-10 12:33:35 +0200

    Add files via upload

---
+++ alerts_1.py
@@ -0,0 +1,30 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Создание экземпляра веб-драйвера
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10, poll_frequency=1)
+
+# Переход на веб-страницу
+driver.get("https://demoqa.com/alerts")
+
+BUTTON_0 = ("xpath", "//button[@class='fc-button fc-cta-consent fc-primary-button']")
+wait.until(EC.element_to_be_clickable(BUTTON_0)).click()
+
+time.sleep(10)
+
+# Клик на кнопку, которая вызывает alert
+
+BUTTON_1 = ("xpath", "//button[@id='alertButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_1)).click()
+
+# Ожидание появления alert и запись его в переменную для дальнейшего взаимодействия
+alert = wait.until(EC.alert_is_present())
+# Переключение на alert
+driver.switch_to.alert
---
+++ alerts_2.py
@@ -0,0 +1,30 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Создание экземпляра веб-драйвера
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10, poll_frequency=1)
+
+# Переход на веб-страницу
+driver.get("https://demoqa.com/alerts")
+
+BUTTON_0 = ("xpath", "//button[@class='fc-button fc-cta-consent fc-primary-button']")
+wait.until(EC.element_to_be_clickable(BUTTON_0)).click()
+
+# time.sleep(10)
+
+# Клик на кнопку, которая вызывает alert
+BUTTON_3 = ("xpath", "//button[@id='confirmButton']")
+wait.until(EC.element_to_be_clickable(BUTTON_3)).click()
+
+alert = wait.until(EC.alert_is_present())
+
+driver.switch_to.alert
+
+alert.dismiss() # Отклоняем алерт
